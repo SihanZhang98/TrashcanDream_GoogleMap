1a8d13618c28d44eacece326ad009bb705c0e1bc
SihanZhang98 <70332584+SihanZhang98@users.noreply.github.com>
2024-11-14 11:39:51 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// const { Map3DElement, Model3DElement } = google.maps.importLibrary("maps3d");
+// const map = document.querySelector('gmp-map-3d')
+// const model = new Model3DElement({
+//     src: 'assets/trashcan.glb',
+//     position: {lat: 40.667, lng: -73.971, altitude: 0},
+//     orientation: {tilt: 270}
+//   });
+
+//   map.append(model);
+
